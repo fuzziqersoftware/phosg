d22812dbab5620c611afa1f749547275e1ff6bb2
Martin Michelsen <mjem@wildblue.net>
2016-10-20 00:20:29 -0700

    remove some "explicit" specifiers

--- Concurrency.hh
+++ Concurrency.hh
@@ -2,12 +2,12 @@
 
 #include <pthread.h>
 
-class rw_lock { // goddamnit why isn't shared_mutex in c++14
+class rw_lock {
 private:
   pthread_rwlock_t lock;
 
 public:
-  explicit rw_lock();
+  rw_lock();
   ~rw_lock();
 
   friend class rw_guard;
@@ -19,6 +19,7 @@ private:
 
 public:
   rw_guard() = delete;
-  explicit rw_guard(rw_lock& lock, bool exclusive);
+  rw_guard(rw_lock& lock, bool exclusive);
   ~rw_guard();
 };
+
