80216fa5669e327da2fc1600b6e5a7d3f5704332
Martin Michelsen <mjem@wildblue.net>
2017-09-20 11:05:05 -0700

    add error string to filesystem exception

--- Filesystem.cc
+++ Filesystem.cc
@@ -28,7 +28,7 @@ unordered_set<string> list_directory(const string& dirname,
 
   DIR* dir = opendir(dirname.c_str());
   if (dir == NULL) {
-    throw runtime_error("can\'t read directory " + dirname);
+    throw runtime_error("can\'t read directory " + dirname + ": " + string_for_error(errno));
   }
 
   struct dirent* entry;
