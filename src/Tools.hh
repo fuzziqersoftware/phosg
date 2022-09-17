4ad25968b18e6a0355ac080589e876b4910fbdf5
Martin Michelsen <fuzziqersoftware+g@gmail.com>
2022-09-17 12:11:47 -0700

    add parallel_range

--- src/Tools.hh
+++ src/Tools.hh
@@ -1,6 +1,14 @@
 #pragma once
 
+#include <stdint.h>
+
 #include <functional>
+#include <vector>
+#include <thread>
+#include <string>
+
+#include "Time.hh"
+#include "Strings.hh"
 
 
 
@@ -14,4 +22,87 @@ private:
 
 inline CallOnDestroy on_close_scope(std::function<void()> f) {
   return CallOnDestroy(move(f));
+}
+
+
+
+template <typename IntT>
+void parallel_range_thread_fn(
+    std::function<bool(IntT, size_t thread_num)>& fn,
+    std::atomic<IntT>& current_value,
+    std::atomic<IntT>& result_value,
+    IntT end_value,
+    size_t thread_num) {
+  IntT v;
+  while ((v = current_value.fetch_add(1)) < end_value) {
+    if (fn(v, thread_num)) {
+      result_value = v;
+      current_value = end_value;
+    }
+  }
+}
+
+// This function runs a function in parallel, using the specified number of
+// threads. If the thread count is 0, the function uses the same number of
+// threads as there are CPU cores in the system. If any instance of the callback
+// returns true, the entire job ends early and all threads stop (after finishing
+// their current call to fn, if any). parallel_range returns the value for which
+// fn returned true, or it returns end_value if fn never returned true. If
+// multiple calls to fn return true, it is not guaranteed which of those values
+// is returned (it is often, but not always, the lowest one).
+template <typename IntT = uint64_t, bool DisplayProgress = true>
+IntT parallel_range(
+    std::function<bool(IntT, size_t thread_num)> fn,
+    IntT start_value,
+    IntT end_value,
+    size_t num_threads = 0) {
+  if (num_threads == 0) {
+    num_threads = std::thread::hardware_concurrency();
+  }
+
+  std::atomic<IntT> current_value(start_value);
+  std::atomic<IntT> result_value(end_value);
+  std::vector<std::thread> threads;
+  while (threads.size() < num_threads) {
+    threads.emplace_back(
+        parallel_range_thread_fn<IntT>,
+        ref(fn),
+        ref(current_value),
+        ref(result_value),
+        end_value,
+        threads.size());
+  }
+
+  if (DisplayProgress) {
+    std::string format_str = "... %08";
+    format_str += printf_hex_format_for_type<IntT>();
+    format_str += " (%s / -%s)\r";
+
+    uint64_t start_time = now();
+    IntT displayed_current_value;
+    while ((displayed_current_value = current_value.load()) < end_value) {
+
+      uint64_t elapsed_time = now() - start_time;
+      std::string elapsed_str = format_duration(elapsed_time);
+
+      std::string remaining_str;
+      if (displayed_current_value) {
+        uint64_t total_time = (elapsed_time << 32) / displayed_current_value;
+        uint64_t remaining_time = total_time - elapsed_time;
+        remaining_str = format_duration(remaining_time);
+      } else {
+        remaining_str = "...";
+      }
+
+      fprintf(stderr, format_str.c_str(), displayed_current_value,
+          elapsed_str.c_str(), remaining_str.c_str());
+      usleep(1000000);
+    }
+  }
+
+  for (auto& t : threads) {
+    t.join();
+  }
+
+  return result_value;
 }
--- src/ToolsTest.cc
+++ src/ToolsTest.cc
@@ -8,6 +8,7 @@ using namespace std;
 
 
 int main(int, char** argv) {
+  printf("-- on_close_scope\n");
   bool called = false;
   {
     auto g1 = on_close_scope([&]() {
@@ -16,6 +17,33 @@ int main(int, char** argv) {
     expect(!called);
   }
   expect(called);
+
+  printf("-- parallel_range\n");
+  constexpr size_t num_threads = 4;
+  vector<size_t> thread_counts(num_threads, 0);
+  auto handle_value = [&](uint64_t, size_t thread_num) -> bool {
+    thread_counts[thread_num]++;
+    return false;
+  };
+  parallel_range<uint64_t, false>(handle_value, 0, 0x10000, num_threads);
+  for (size_t x = 0; x < num_threads; x++) {
+    fprintf(stderr, "---- thread %zu: %zu\n", x, thread_counts[x]);
+    expect_ne(thread_counts[x], 0);
+  }
+
+  printf("-- parallel_range return value\n");
+  uint64_t target_value = 0xC349;
+  auto is_equal = [&](uint64_t v, size_t) -> bool {
+    return (v == target_value);
+  };
+  expect_eq((parallel_range<uint64_t, false>(is_equal, 0, 0x10000, num_threads)), target_value);
+  // Note: We can't check that parallel_range ends early when fn returns true
+  // because it's not actually guaranteed to do so - it's only guaranteed to
+  // return any of the values for which fn returns true. One could imagine a sequence of events in
+  // which the target value's call takes a very long time, and all other threads
+  // could finish checking all other values before the target one returns true.
+  target_value = 0xCC349; // > end_value; should not be found
+  expect_eq((parallel_range<uint64_t, false>(is_equal, 0, 0x10000, num_threads)), 0x10000);
 
   printf("%s: all tests passed\n", argv[0]);
   return 0;
