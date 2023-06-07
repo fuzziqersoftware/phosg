99bf22ce7a019eb6b36d97524c9f91cb5275e388
Martin Michelsen <fuzziqersoftware+g@gmail.com>
2023-06-06 21:45:01 -0700

    satisfy gcc's complaints about enum_for_name

--- src/Tools.hh
+++ src/Tools.hh
@@ -4,6 +4,7 @@
 
 #include <atomic>
 #include <functional>
+#include <stdexcept>
 #include <string>
 #include <thread>
 #include <vector>
@@ -114,9 +115,11 @@ IntT parallel_range(
 template <typename T>
 T enum_for_name(const char*) {
   static_assert(always_false<T>::v, "unspecialized enum_for_name should never be called");
+  throw std::logic_error("unspecialized enum_for_name should never be called");
 }
 
 template <typename T>
 const char* name_for_enum(T) {
   static_assert(always_false<T>::v, "unspecialized name_for_enum should never be called");
+  throw std::logic_error("unspecialized name_for_enum should never be called");
 }
