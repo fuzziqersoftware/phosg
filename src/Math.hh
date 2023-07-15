9b9b7f6f4844f8a98542637a8de048da1d667a6a
Martin Michelsen <fuzziqersoftware+g@gmail.com>
2023-07-15 13:38:16 -0700

    add log2i

--- src/Math.hh
+++ src/Math.hh
@@ -1,7 +1,7 @@
 #include <utility>
 
 template <typename IntT>
-IntT gcd(IntT a, IntT b) {
+constexpr IntT gcd(IntT a, IntT b) {
   if (b == 0) {
     return a;
   }
@@ -9,7 +9,12 @@ IntT gcd(IntT a, IntT b) {
 }
 
 template <typename IntT>
-std::pair<IntT, IntT> reduce_fraction(IntT a, IntT b) {
+constexpr std::pair<IntT, IntT> reduce_fraction(IntT a, IntT b) {
   IntT denom = gcd(a, b);
   return std::make_pair(a / denom, b / denom);
+}
+
+template <typename IntT>
+constexpr IntT log2i(IntT v) {
+  return (sizeof(IntT) << 3) - 1 - __builtin_clz(v);
 }
